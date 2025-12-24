a3fa7c6939ac5a5617117495fbcdc91ba69cf0e8
NullOS-project <NullOSDev@gmail.com>
2025-12-24 14:55:43 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright [2025] [name of copyright owner]
+# Copyright 2025 NullOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
